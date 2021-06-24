a51e29d16b8bc5f889336205703a9c5c768f4ce2
Gregor Laan <laangregor@gmail.com>
2021-06-24 20:17:06 +0300

    Remove helloWorld, Add Quotes with dummy data

---
+++ src/components/Quotes.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>
+      Quotes
+    </h1>
+
+    <ul>
+      <li 
+        v-for="quote in quotes" 
+        :key="quote"
+      >
+        {{ quote.desc }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+  import quotes from "../data/quotes.json" 
+</script>
+
+<style scoped>
+</style>
